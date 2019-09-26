e70eb88c44b668ee1d6c92952be801ae3f9ee122
qbuser <afsals@qburst.com>
2019-09-26 15:47:12 +0530

    Bug fix

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-server-config-generator',
     url='https://github.com/Afsal-qburst/django-server-config-generator',
-    version='0.8',
+    version='0.9',
     packages=find_packages(),
     include_package_data=True,
     description='A simple Django app to create server config',
